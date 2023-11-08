d27fba2f34890d813a53964a3ad29402dfd3657a
VanAnhk3 <pvananh777@gmail.com>
2023-11-08 15:01:01 +0700

    Slide25

---
+++ Data/PHP/slide21.php
@@ -0,0 +1,8 @@
+<?php
+$longAccount = 10.000.000;
+echo "sos tien trong tai khoan long: $longAccount";
+$HUMGPay = 10.000.000;
+echo "HUMG chuyen tien: $HUMGPay";
+&$longAccount = $longAccount + $HUMGPay;
+echo "So tien trong tai khoan long: $longAccount";
+?>
---
+++ Data/PHP/slide25.php
@@ -0,0 +1,11 @@
+<?php
+$Subject = "Lap trinh ung dung web";
+$unversityNam ="HUMG";
+$firstNumber = 2;
+$secondtNumber = 2;
+
+echo"Hoc".$Subject."o phong 502 tai truong ".$unversityName."<br>";
+echo"Hoc $Subject o phong 502 tai truong $unversityName <br>";
+echo"<h1> $unversityName </h1>";
+echo $firsrNumber + $secondtNumber;
+?>
